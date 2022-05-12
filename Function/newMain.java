e0aab032c74eb711e2d9cd4240ecb02a5af05461
gVertex <magia_destructor@gmx.de>
2022-05-12 09:56:21 +0200

    added calculation of need time, Deleted main.java

---
+++ Function/newMain.java
@@ -0,0 +1,14 @@
+import Controller.Calculator;
+
+public class newMain {
+
+	public static void main(String[] args) {
+		
+		int startHour,endHour,startMinute,endMinute;
+		//Die methode zum berechnen der benötigten Zeit
+		//Calculator.TimerCalculator(startHour, startMinute,endHour,endMinute);
+	}
+
+	
+
+}
