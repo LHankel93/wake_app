fd1c1adf2472fd6e79480ca4de6c089dbe2223d8
gVertex <magia_destructor@gmx.de>
2022-05-12 15:56:46 +0200

    spacebar

--- Function/newMain.java
+++ Function/newMain.java
@@ -10,10 +10,9 @@ public class newMain {
 		//
 		int arriveTimeHour=0,arriveTimeMinute=0,travelTimeHour=0,travelTimeMinute=0,preperationTimeHour=0,preperationTimeMinute=0;
 		//Die methode zum berechnen der benötigten Zeit
-		Hauptansicht mainWindow = new Hauptansicht();
-
-		
+		Hauptansicht mainWindow = new Hauptansicht();		
 		Calculator.TimerCalculator();
+		
 	}
  
 	
