20f384d4b2e08bfdde7392ee5dace1575db2c9bc
Lorenz_Hankel <lorenz.hankel@yahoo.de>
2022-05-14 11:31:16 +0200

    Commit und Push to Branch Lorenz
    -Getter & Setter für Wecker hinzugefügt
    -JavaDoc erstellt

--- Function/model/Wecker.java
+++ Function/model/Wecker.java
@@ -1,6 +1,7 @@
 package model;
 
 /**
+ * Diese Klasse soll Wecker Objekte erstellen können.
  * 
  * @author Lorenz Hankel
  * @version v0.0.1
@@ -56,8 +57,163 @@ public class Wecker {
 		if (weckzeitStunden == null || weckzeitMinuten == null) {
 			this.weckzeitMinuten = 0;
 			this.weckzeitStunden = 0;
+		} else {
+			this.weckzeitStunden = weckzeitStunden;
+			this.weckzeitMinuten = weckzeitMinuten;
 		}
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the ankunftStunden
+	 */
+	public int getAnkunftStunden() {
+		return ankunftStunden;
+	}
+
+	/**
+	 * @return the ankunftMinuten
+	 */
+	public int getAnkunftMinuten() {
+		return ankunftMinuten;
+	}
+
+	/**
+	 * @return the fahrzeitStunden
+	 */
+	public int getFahrzeitStunden() {
+		return fahrzeitStunden;
+	}
+
+	/**
+	 * @return the fahrzeitMinuten
+	 */
+	public int getFahrzeitMinuten() {
+		return fahrzeitMinuten;
+	}
+
+	/**
+	 * @return the vorbereitungStunden
+	 */
+	public int getVorbereitungStunden() {
+		return vorbereitungStunden;
+	}
+
+	/**
+	 * @return the vorbereitungMinuten
+	 */
+	public int getVorbereitungMinuten() {
+		return vorbereitungMinuten;
+	}
+
+	/**
+	 * @return the zielOrt
+	 */
+	public String getZielOrt() {
+		return zielOrt;
+	}
+
+	/**
+	 * @return the startOrt
+	 */
+	public String getStartOrt() {
+		return startOrt;
+	}
+
+	/**
+	 * @return the weckzeitStunden
+	 */
+	public int getWeckzeitStunden() {
+		return weckzeitStunden;
+	}
+
+	/**
+	 * @return the weckzeitMinuten
+	 */
+	public int getWeckzeitMinuten() {
+		return weckzeitMinuten;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param ankunftStunden the ankunftStunden to set
+	 */
+	public void setAnkunftStunden(int ankunftStunden) {
+		this.ankunftStunden = ankunftStunden;
+	}
+
+	/**
+	 * @param ankunftMinuten the ankunftMinuten to set
+	 */
+	public void setAnkunftMinuten(int ankunftMinuten) {
+		this.ankunftMinuten = ankunftMinuten;
+	}
+
+	/**
+	 * @param fahrzeitStunden the fahrzeitStunden to set
+	 */
+	public void setFahrzeitStunden(int fahrzeitStunden) {
+		this.fahrzeitStunden = fahrzeitStunden;
+	}
+
+	/**
+	 * @param fahrzeitMinuten the fahrzeitMinuten to set
+	 */
+	public void setFahrzeitMinuten(int fahrzeitMinuten) {
+		this.fahrzeitMinuten = fahrzeitMinuten;
+	}
+
+	/**
+	 * @param vorbereitungStunden the vorbereitungStunden to set
+	 */
+	public void setVorbereitungStunden(int vorbereitungStunden) {
+		this.vorbereitungStunden = vorbereitungStunden;
+	}
+
+	/**
+	 * @param vorbereitungMinuten the vorbereitungMinuten to set
+	 */
+	public void setVorbereitungMinuten(int vorbereitungMinuten) {
+		this.vorbereitungMinuten = vorbereitungMinuten;
+	}
+
+	/**
+	 * @param zielOrt the zielOrt to set
+	 */
+	public void setZielOrt(String zielOrt) {
+		this.zielOrt = zielOrt;
+	}
+
+	/**
+	 * @param startOrt the startOrt to set
+	 */
+	public void setStartOrt(String startOrt) {
+		this.startOrt = startOrt;
+	}
+
+	/**
+	 * @param weckzeitStunden the weckzeitStunden to set
+	 */
+	public void setWeckzeitStunden(int weckzeitStunden) {
 		this.weckzeitStunden = weckzeitStunden;
+	}
+
+	/**
+	 * @param weckzeitMinuten the weckzeitMinuten to set
+	 */
+	public void setWeckzeitMinuten(int weckzeitMinuten) {
 		this.weckzeitMinuten = weckzeitMinuten;
 	}
 
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"Controller","l":"Calculator"},{"p":"Controller","l":"CSVHandler"},{"p":"view","l":"Hauptansicht"},{"p":"<Unnamed>","l":"newMain"},{"p":"model","l":"Wecker"}];updateSearchResults();
